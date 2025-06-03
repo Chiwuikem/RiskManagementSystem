8d34f375469cfd9b7c0c6a6143ba73182bde4224
Chiwuikem1 <corji@villanova.edu>
2025-06-03 18:02:43 -0400

    Set UP
    Initializied spring boot

---
+++ risk-management-system/risk-management-system/src/main/java/com/riskinsights/risk/risk_management_system/RiskManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.riskinsights.risk.risk_management_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication(scanBasePackages = "com.riskinsights.risk")
+public class RiskManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RiskManagementSystemApplication.class, args);
+	}
+
+}
---
+++ risk-management-system/risk-management-system/src/test/java/com/riskinsights/risk/risk_management_system/RiskManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.riskinsights.risk.risk_management_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RiskManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
